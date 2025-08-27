697b097a73dfe9710736f0301ce1621b4e1176a8
Kotturi858 <kotturi858bharath@gmail.com>
2025-08-27 12:40:43 +0530

    add cloud library

--- src/main/java/com/support/service/support_service/SupportServiceApplication.java
+++ src/main/java/com/support/service/support_service/SupportServiceApplication.java
@@ -2,8 +2,10 @@ package com.support.service.support_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SupportServiceApplication {
 
 	public static void main(String[] args) {
