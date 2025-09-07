b4fd7ebe901c2480f90cc013d90f9839dabdb3c5
Kotturi858 <kotturi858bharath@gmail.com>
2025-09-07 13:32:28 +0530

    add custom config file for demonstration

--- src/main/java/com/support/service/support_service/SupportServiceApplication.java
+++ src/main/java/com/support/service/support_service/SupportServiceApplication.java
@@ -2,14 +2,20 @@ package com.support.service.support_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SupportServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SupportServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SupportServiceApplication.class, args);
+
+        //to define custom application.properties file
+//        new SpringApplicationBuilder(SupportServiceApplication.class)
+//                .properties("spring.config.name=custom-config")
+//                .run(args);
+    }
 
 }
